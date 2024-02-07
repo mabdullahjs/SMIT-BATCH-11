ea7f60462f34bbb20b3726a25538ce26cc452311
mabdullahjs <mabdullah2037@outlook.com>
2024-02-07 10:47:13 +0500

    JS Intro

---
+++ 02-Javascript/01-Introduction/app.js
@@ -0,0 +1,147 @@
+// var a = 10;
+// var b = 20;
+
+// console.log(b);
+
+// var myFirstName = 20;
+// var lastName = 2;
+
+
+// Data types
+
+
+// string
+// Number
+// Boolean
+
+
+// var firstName = "Muhammad Abdullah"; 
+// var age = 20;
+// var isUser = 'false'
+
+// typeof()
+// alert()
+
+
+
+// console.log(firstName);
+// console.log(age);
+// console.log(isUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstName = "Muhammad"; 
+// var lastName = "Abdullah"
+// var age = 20;
+// var isUser = false
+
+
+// // console.log(typeof(isUser))
+// // console.log(firstName , age , isUser);
+
+
+// console.log("My name is " + firstName + " and my age is " + age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstName = "Muhammad";
+// var lastName = "Abdullah"
+// var age = 20;
+
+// console.log('My name is ' + firstName + " " + lastName + ' and my age is ' + age);
+
+
+// var firstName = 'abdullah'
+// alert('Hello how are you?' + firstName);
+
+
+// var alert = prompt('Enter your name?');
+// console.log(userName);
+
+
+
+
+
+
+// alert Assingment
+
+// question 1
+
+// alert('hello world');
+
+
+
+//question 2
+
+// alert('error message')
+
+
+
+
+
+
+
+
+
+
+
+
+
