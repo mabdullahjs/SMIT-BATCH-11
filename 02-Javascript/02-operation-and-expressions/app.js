f3e4aa08ff434f1096bead937ddaf8a2946cb2f2
mabdullahjs <mabdullah2037@outlook.com>
2024-02-23 10:47:02 +0500

    operation and expression

---
+++ 02-Javascript/02-operation-and-expressions/app.js
@@ -0,0 +1,134 @@
+// console.log('hello world');
+
+// var userName = 'abdullah';
+// console.log(userName)
+
+
+// alert('hello world');
+// alert('hello world');
+
+
+// var num1 = 10;
+// var num2 = '20';
+// var num3 = 20;
+
+// console.log(typeof(num1))
+// console.log(num1 + num2 + num3)
+
+
+
+
+
+// var username = prompt('Enter your name!');
+// console.log(username);
+
+
+
+
+
+// var num1 = Number(prompt('Enter first number'));
+// var num2 = Number(prompt('Enter second number'));
+
+
+// var num1 = +prompt('Enter first number');
+// var num2 = +prompt('Enter second number');
+// console.log(num1 + num2);
+
+
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// % == modulus
+
+// var num1 = 10;
+// var num2 = 5;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log('modulus ===> ', num1 % num2);
+// console.log(10 + 20 - 10 * 5 / 2);
+
+
+
+
+
+
+
+
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+// var num1 = 12;
+// num1 = num1 + 10;
+// num1 += 10;
+// num1 -= 10;
+// num1 *= 10;
+// num1 /= 10;
+// console.log(num1);
+
+
+
+//Comparision Operator
+
+// >
+// <
+// == || !=
+// === || !==
+// != data type check nahi kraiga!
+// !== data type check kraiga!
+// >=
+// <=
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// console.log(10 != 10);
+// console.log(10 == '10');
+// var username = 'abdullah'
+// document.write('<h1>'+ username+ '</h1>');
+
+
+
+
+//logical Operator
+
+//&& == and
+//|| == OR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
