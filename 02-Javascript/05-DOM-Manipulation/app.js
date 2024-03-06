4d347f072ebc3305b692890188bb9f19b1e8ca1d
mabdullahjs <mabdullah2037@outlook.com>
2024-03-06 10:42:21 +0500

    dom manipulation practice

--- 02-Javascript/05-DOM-Manipulation/app.js
+++ 02-Javascript/05-DOM-Manipulation/app.js
@@ -98,3 +98,83 @@
 
 
 
+// var head = document.querySelector('#head');
+// head.innerHTML = "change through js"
+// console.log(head.innerHTML);
+
+
+
+// var list = document.querySelector('#list');
+// list.innerHTML = '<li>Home</li>'
+
+
+// function changeList() {
+//     list.innerHTML = `<li>Javascript</li>
+//     <li>Python</li>
+//     <li>PHP</li>`
+// }
+
+
+
+
+
+
+// var input = document.querySelector('#input');
+// console.log(input.value);
+
+// function getValue (){
+//     input.value = ''
+//     console.log(input.value);
+// }
+
+
+var chemMarks = document.querySelector('#chem-marks')
+var phyMarks = document.querySelector('#phy-marks')
+var mathMarks = document.querySelector('#math-marks')
+var comMarks = document.querySelector('#com-marks')
+var studentTotal = document.querySelector('#total-marks')
+var percentage = document.querySelector('#percentage')
+var grade = document.querySelector('#grade')
+
+
+function calculatePercentage() {
+    console.log(chemMarks.value);
+    console.log(phyMarks.value);
+    console.log(mathMarks.value);
+    console.log(comMarks.value);
+
+    var totalMarks = 400;
+    var obtainedMarks = +chemMarks.value + +phyMarks.value + +mathMarks.value + +comMarks.value;
+    console.log('obtained marks ===>', obtainedMarks);
+    studentTotal.innerHTML = obtainedMarks;
+
+    var studentPercentage = obtainedMarks / totalMarks * 100;
+    percentage.innerHTML = studentPercentage;
+
+
+    if(studentPercentage > 80){
+        grade.innerHTML = 'A+'
+    }else if(studentPercentage > 70){
+        grade.innerHTML = 'A'
+    } else if(studentPercentage > 60){
+        grade.innerHTML = 'B'
+    }else{
+        grade.innerHTML = 'FAIL'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
