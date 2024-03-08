002e016fc2c1fa3a485cd99a4b51b42733559bde
mabdullahjs <mabdullah2037@outlook.com>
2024-03-08 10:31:08 +0500

    array

--- 02-Javascript/06-Array/app.js
+++ 02-Javascript/06-Array/app.js
@@ -1,1 +1,234 @@
-console.log('hello arrays');
+// console.log('hello arrays');
+
+// let
+// const
+
+
+// var username = 'abdullah'; // declare
+// var username = 'usman';// redeclared
+// username = 'ammar'; // reassign
+
+// console.log(username)
+
+
+
+// var
+// redeclare
+// reassign
+
+
+
+
+
+
+
+// let username;
+// username = 'abdullah'
+
+// let username = 'abdullah';
+// let username = 'ammar'
+// username = 'usman'
+// console.log(username)
+
+
+
+// const username = 'abdullah';
+// username = 'usman'
+// console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let city1 = 'karachi';
+// let city2 = 'lahore';
+// let city3 = 'islamabad';
+
+
+// let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur'];
+// let cities = ['karachi' , 'lahore' , 'islamabad' , 'faislabad' , 'hyderabad' , 'sheikarpur' , 20 , true , ['fb area' , 'liyari' , 'golimar']];
+// console.log(cities[8][2]);
+// console.log(cities.length);
+// console.log(cities[3]);
+// console.log(typeof(cities))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const user = ['abdullah' , 'usman' , 'haider'];
+// // console.log(`${user[0]} ${user[2]}`);
+
+// user[0] = 'ammar';
+// user[4] = 'ali'
+// console.log(user[3])
+
+
+
+
+
+
+
+
+
+
+
+
+// const user = [];
+// user[0] = prompt('enter name1')
+// user[1] = prompt('enter name2')
+// user[2] = prompt('enter name3')
+// console.log(user);
+
+
+
+
+
+
+// const fruit = 'apple';
+// const fruits = ['banana' , 'orange', 'mango' , fruit];
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+// const fruits = ['banana' , 'orange', 'mango'];
+// fruits.push('watermelon');
+// fruits.pop();
+// fruits.unshift('watermelon')
+// fruits.shift()
+
+// fruits.splice('shurw kaha sa krna ha' , 'kitna delete krna hain' , 'uski jaga kiya rkhna haa')
+// fruits.splice(2 , 1 , 'strawberry' , 'watermelon' , 'melon' , 'mangoes' , 'guava' , 'nashpati' , 'grapes' , 'anaaar' , 'kinno');
+// // fruits.splice(1 , 3);
+// fruits.splice(6 , 2 , 'banana');
+// fruits[3].pop()
+// console.log(fruits)
+
+
+// const newArr = fruits.slice(3)
+// console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const names = 'Muhammad Abdullah khan';
+// const arr = names.split(' ')
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+// const names = ['Muhammad' , 'Abdullah' , 'Khan'];
+// const str = names.join(' ')
+// console.log(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
