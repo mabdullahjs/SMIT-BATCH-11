da6c3ee643ba2bf0bbd961820283cb4d8cd85a38
mabdullahjs <mabdullah2037@outlook.com>
2024-03-25 12:31:58 +0500

    revision

--- 02-Javascript/09-Revision/app.js
+++ 02-Javascript/09-Revision/app.js
@@ -1,37 +1,171 @@
-const input = document.querySelector('#exampleInputEmail1');
-const div = document.querySelector('#todos');
+// const input = document.querySelector('#exampleInputEmail1');
+// const div = document.querySelector('#todos');
 
-const arr = [];
-function addTodo() {
-    div.innerHTML = ''
-    arr.push(input.value);
-    console.log(arr);
+// const arr = [];
+// function addTodo() {
+//     div.innerHTML = ''
+//     arr.push(input.value);
+//     console.log(arr);
 
-    for (let i = 0; i < arr.length; i++){
-        div.innerHTML += `
-        <div class="container border p-4 rounded mt-2 d-flex justify-content-between align-items-center mt-3">
-            <div>
-                <h1>${arr[i]}</h1>
-            </div>
-            <div>
-                <button onclick="deleteTodo(${i})" type="button" class="btn btn-danger text-white"><i
-                        class="fa-solid fa-trash text-white"></i></button>
-                <button onclick="editTodo(${i})" type="button" class="btn btn-success text-white"><i
-                        class="fa-solid fa-pen-to-square text-white"></i></button>
-            </div>
-        </div>
-        `
-    }
-}  
+//     for (let i = 0; i < arr.length; i++){
+//         div.innerHTML += `
+//         <div class="container border p-4 rounded mt-2 d-flex justify-content-between align-items-center mt-3">
+//             <div>
+//                 <h1>${arr[i]}</h1>
+//             </div>
+//             <div>
+//                 <button onclick="deleteTodo(${i})" type="button" class="btn btn-danger text-white"><i
+//                         class="fa-solid fa-trash text-white"></i></button>
+//                 <button onclick="editTodo(${i})" type="button" class="btn btn-success text-white"><i
+//                         class="fa-solid fa-pen-to-square text-white"></i></button>
+//             </div>
+//         </div>
+//         `
+//     }
+// }  
 
 
 
-function deleteTodo(index){
-    console.log('todo deleted' , index);
-}
-function editTodo(index){
-    console.log('todo edited' , index);
-}
+// function deleteTodo(index){
+//     console.log('todo deleted' , index);
+// }
+// function editTodo(index){
+//     console.log('todo edited' , index);
+// }
+
+
+
+
+
+
+
+
+
+
+//string
+//number
+//boolean
+
+//object
+
+
+// let username = 'abdullah';
+// const age = 20;
+
+
+
+// console.log('2' + '2');
+// console.log(`my name is ${username} and my age is ${age}`);
+
+
+// const age = 17;
+// if(age < 18 || age > 20){
+//     console.log('if block');
+// }
+// else if(){
+
+// }
+// else{
+//     console.log('else block');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const h1 = document.querySelector('#head');
+// h1.innerHTML = 'changed through javascript'
+// console.log(h1.innerHTML);
+
+
+
+
+
+
+
+
+
+// let username = "haseeb"
+// for(let i = 0; i < 10; i++){
+//     console.log(username , i);
+// }
+
+// const arr=[`apple `,`orange`,`grapes`,` strawberry`]
+
+// for(i=1; i<=arr.length;)
+
+// const ul = document.querySelector('ul');
+// for (let i = 1; i <= 10; i++){
+//     console.log(`5 * ${i} = ${5*i}`);
+//     ul.innerHTML += `<li>5 * ${i} = ${5*i}</li>`
+// }
+
+
+
+
+
+
+
+
+
+
+// let num = 10;
+// for (let i = num; i >= 1; i--) {
+//     let str = '';
+//     for (let j = 0; j < i; j++) {
+//         str += '*'
+//     }
+//     console.log(str);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 10;
+// let str = ''
+
+// for (let i = 0; i < num; i++) {
+//     str += '*';
+//     console.log(str);
+// }
+// for (let i = 0; i > num; i++) {
+//     str += '*';
+//     console.log(str);
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
