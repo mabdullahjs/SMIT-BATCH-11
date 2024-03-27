820bd3a10375bc5ef3a00eae3d18ed259ce05878
mabdullahjs <mabdullah2037@outlook.com>
2024-03-27 12:28:10 +0500

    loops

--- 02-Javascript/07-Loops/app.js
+++ 02-Javascript/07-Loops/app.js
@@ -244,9 +244,15 @@
 
 
 
+// const arr = ['apple' , 'banana' , 'orange']
 
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//     if(arr[i] === 'banana'){
+//         break
+//     }    
 
-
+// }
 
 
 
