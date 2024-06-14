149b0fb188d0b85a55aebb142ba7906333027f3f
mabdullahjs <bilalkhanshakir@gmail.com>
2024-06-14 11:00:58 +0500

    weather app

--- 02-Javascript/21-Weather-App/app.js
+++ 02-Javascript/21-Weather-App/app.js
@@ -1,18 +1,31 @@
-console.log("weather app");
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=YOUR_API_KEY
+// b4db485d7c4c485fa6d84351232508
+
+// http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=London&aqi=no
 
 const form = document.querySelector("#form");
-const input = document.querySelector("#input");
+const city = document.querySelector("#city");
+const card = document.querySelector("#card");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   axios(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${input.value}?key=YOUR_API_KEY`
+    `http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no`
   )
     .then((res) => {
       console.log(res.data);
+      card.innerHTML = `
+      <div class='bg-white border border-gray-800 shadow-lg ring ring-gray-700 ring-opacity-50 w-50 mx-auto mt-5 rounded-lg p-4 mb-5'>
+            <h1 class='text-2xl'>${res.data.location.name}</h1>
+            <p class='text-gray-500'>${res.data.location.localtime} ,${res.data.location.country} </p>
+            <div class='mt-5 d-flex justify-content-between align-items-center mx-5 pb-4'>
+                <h2 class='text-4xl md:text-7xl lg:text-8xl'>${res.data.current.temp_c}°C</h2>
+                <img width="160px" src=${res.data.current.condition.icon} alt='weatherImg' />
+            </div>
+            <h4>${res.data.current.condition.text}</h4>
+        </div>
+      `;
     })
     .catch((err) => {
-      console.log(err);
+      console.log("error===>", err);
     });
 });
