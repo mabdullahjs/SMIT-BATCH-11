e224906c55e87eab9521d1e0bcace2f7860e4cc6
mabdullahjs <mabdullah2037@outlook.com>
2024-10-28 10:43:58 +0500

    map

--- 07-React-Native/04-tailwind-css/app/index.tsx
+++ 07-React-Native/04-tailwind-css/app/index.tsx
@@ -15,7 +15,7 @@
 
 import { useState, useEffect } from 'react';
 import { Platform, Text, View, StyleSheet, TextInput, TouchableOpacity, FlatList, ScrollView } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 interface SinglePlace {
@@ -32,8 +32,10 @@ export default function App() {
   const [location, setLocation] = useState<null | any>(null);
   const [errorMsg, setErrorMsg] = useState<null | string>(null);
   const [search, setSearch] = useState('')
-  const [places , setPlaces] = useState<null | AllPlaces[]>(null);
-  const [singlesearchPlace , setsinglesearchPlace] = useState<null| SinglePlace>(null);
+  const [places, setPlaces] = useState<null | AllPlaces[]>(null);
+  const [singlesearchPlace, setsinglesearchPlace] = useState<null | SinglePlace>(null);
+  const [region, setRegion] = useState<any>(null);
+  const [direction , setDirection] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
@@ -46,6 +48,12 @@ export default function App() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      })
       console.log(location);
 
     })();
@@ -79,7 +87,6 @@ export default function App() {
 
       .then(res => res.json())
       .then(res => {
-        console.log(res.results)
         setPlaces(res.results)
       })
       .catch(err => console.error(err));
@@ -88,11 +95,17 @@ export default function App() {
 
   // single place
 
-  const singlePlace = (item: any)=>{
+  const singlePlace = (item: any) => {
     setPlaces(null);
     setsinglesearchPlace({
       latitude: item.geocodes.main.latitude,
       longitude: item.geocodes.main.longitude
+    })
+    setRegion({
+      latitude: item.geocodes.main.latitude,
+      longitude: item.geocodes.main.longitude,
+      latitudeDelta: 0.001,
+      longitudeDelta: 0.001,
     })
 
   }
@@ -110,21 +123,16 @@ export default function App() {
         <Text>Search</Text>
       </TouchableOpacity>
       {places && <FlatList
-         data={places}
-         renderItem={({item}: {item: {name: string}}) => {
+        data={places}
+        renderItem={({ item }: { item: { name: string } }) => {
           return <View style={styles.list}>
-            <Text onPress={()=>singlePlace(item)}>{item.name}</Text>
+            <Text onPress={() => singlePlace(item)}>{item.name}</Text>
           </View>
-         }}
-         keyExtractor={(item:{fsq_id: string}) => item.fsq_id
-         }
-        />}
-      {location && <MapView style={styles.map} initialRegion={{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.0001,
-        longitudeDelta: 0.0001,
-      }}>
+        }}
+        keyExtractor={(item: { fsq_id: string }) => item.fsq_id
+        }
+      />}
+      {location && <MapView region={region} onRegionChangeComplete={setRegion} style={styles.map}>
         <Marker coordinate={{
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
@@ -133,8 +141,16 @@ export default function App() {
           latitude: singlesearchPlace.latitude,
           longitude: singlesearchPlace.longitude,
         }} />}
+        {singlesearchPlace && direction && <Polyline coordinates={[{latitude: location.coords.latitude , longitude: location.coords.longitude},{
+          latitude: singlesearchPlace.latitude,
+          longitude: singlesearchPlace.longitude
+        }]} strokeWidth={5} strokeColor='#000000'/>}
 
       </MapView>}
+
+      <TouchableOpacity onPress={()=>setDirection(!direction)} style={styles.button}>
+        <Text>Direction</Text>
+      </TouchableOpacity>
     </View>
   );
 }
